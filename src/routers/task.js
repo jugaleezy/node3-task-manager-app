888dbd020c0239fa640e6efe45553a8721ce5405
Jugaleezy <helloworld.ea.21c@gmail.com>
2019-05-21 17:53:29 +0530

    added paginating task data feature

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,6 +20,7 @@ router.post('/tasks', auth, async (req,res) => {
 })
 
 //GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
 router.get('/tasks', auth, async (req,res) => {
     const match = {}
 
@@ -30,7 +31,11 @@ router.get('/tasks', auth, async (req,res) => {
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         const tasks = req.user.tasks
         res.send(tasks)

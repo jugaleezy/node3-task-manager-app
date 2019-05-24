07b9a778621f16001225ca3e35cf11afd3baaec4
Jugaleezy <helloworld.ea.21c@gmail.com>
2019-05-24 18:21:26 +0530

    added file upload feature with validating, handling error, auto-cropping and formatting image

--- src/index.js
+++ src/index.js
@@ -8,6 +8,31 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter (req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)/)) {
+            return cb(new Error('please upload a word document.'))
+        }
+        cb(undefined, true)
+    }
+})
+
+// const errorMiddleware = (req, res, next) => {
+//     throw new Error('from error middleware')
+// }
+
+app.post('/upload', upload.single('upload'), (req,res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -16,19 +41,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server started on port '+port)
 })
-
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-
-// const main = async () => {
-//     // const task = await Task.findById('5cde5eb2797f7f8788a1f310')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('5cde5e44841e9788b417d482')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-// main()
